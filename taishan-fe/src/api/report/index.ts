1fa498aadc4faa37f766dbc6cbd9f072f5427f14
熊林涛 <xionglintao@cdfsunrise.com>
2025-05-17 17:31:07 +0800

    scene代码提交

--- taishan-fe/src/api/report/index.ts
+++ taishan-fe/src/api/report/index.ts
@@ -7,16 +7,8 @@ class SceneReportService {
     return res.data
   }
 
-  getReportRpsList(data) {
-    return request.post({ url: `/${domain}/report/rpsList`, data })
-  }
-
-  // editRunningStepRps(data) {
-  //   return request.post({ url: `/${domain}/report/rpsModify`, data })
-  // }
-
   reportRpsModify(data) {
-    return request.post({ url: `/${domain}/report/reportRpsModify`, data })
+    return request.post({ url: `/${domain}/report/rps/modify`, data })
   }
 }
 
